725829e91f276a23d297256ad34bdd41db808ad9
Florent Angly <florent.angly@gmail.com>
2014-03-12 13:53:33 +1000

    Added module documentation

--- bpipe/shared.groovy
+++ bpipe/shared.groovy
@@ -1,6 +1,10 @@
 @Transform("mcf")
 mapping2mcf = {
-   doc title: "Create a 454 MID config file from a QIIME mapping file"
+   doc title: "Create a 454 MID config file from a QIIME mapping file",
+       desc:  """Parameters:
+                    none""",
+       constraints: "",
+       author: "Florent Angly (florent.angly@gmail.com)"
    exec """
       echo -e "custom_MIDs\n{" > $output.mcf &&
       cut -f 1,2 $input.mapping | grep -v ^# | perl -p -e 's/^(.+)\t(.+)\$/mid = "\$1", "\$2", 2;/;' >> $output.mcf &&
@@ -10,7 +14,11 @@ mapping2mcf = {
 
 
 split_sff_libraries = {
-   doc title: "Use MIDs to separate SFF records from different samples"
+   doc title: "Use MIDs to separate SFF records belonging to different samples",
+       desc:  """Parameters:
+                    none""",
+       constraints: "",
+       author: "Florent Angly (florent.angly@gmail.com)"
    produce("${input.prefix}*.sff") {
       exec """
          module load 454 &&
@@ -22,7 +30,11 @@ split_sff_libraries = {
 
 @Transform("fastq")
 sff2fastq = {
-   doc title: "Convert an SFF file to FASTQ"
+   doc title: "Convert an SFF file to FASTQ",
+       desc:  """Parameters:
+                    none""",
+       constraints: "",
+       author: "Florent Angly (florent.angly@gmail.com)"
    exec """
       module load sff2fastq &&
       sff2fastq -o $output.fastq $input.sff
@@ -32,7 +44,11 @@ sff2fastq = {
 
 @Filter("rename_seqs")
 rename_seqs = {
-   doc title: "Prefix FASTQ sequence names based on their filename"
+   doc title: "Prefix FASTQ sequence names based on their filename",
+       desc:  """Parameters:
+                    none""",
+       constraints: "",
+       author: "Florent Angly (florent.angly@gmail.com)"
    // From a file like "Gasket76.split_libraries.3833.fastq", use the "3833" prefix
    // A sequence called "HN758CG01AIWK2" would thus become "3833_HN758CG01AIWK2"
    // In comparison, QIIME would call it "3833_1"
@@ -47,7 +63,11 @@ rename_seqs = {
 
 @Filter("cat_files")
 cat_files = {
-   doc title: "Concatenate all files into a single one"
+   doc title: "Concatenate all files into a single one",
+       desc:  """Parameters:
+                    none""",
+       constraints: "",
+       author: "Florent Angly (florent.angly@gmail.com)"
    exec """
       cat $inputs > $output
    """
@@ -55,6 +75,11 @@ cat_files = {
 
 
 final_report = {
+   doc title: "Report the name of output files",
+       desc:  """Parameters:
+                    none""",
+       constraints: "",
+       author: "Florent Angly (florent.angly@gmail.com)"
    println("The outputs are : "+inputs)
    //forward inputs
 }
